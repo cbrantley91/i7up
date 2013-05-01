3f1398569441b8603df22b6d1f4c4bdcb57822b2
Chad Brantley <cbrantley@Tentomon.(none)>
2013-05-01 03:00:47 -0700

    Fixed some stuff regarding views.

--- mysite/i7up/views.py
+++ mysite/i7up/views.py
@@ -7,13 +7,32 @@ import base.i7up as i7
 
 def results(request):
     #p = get_object_or_404(MyPoll, pk=poll_id)
-    i7code = request.POST['i7code']
+    i7code = None
+    if 'i7code' in request.POST:
+        i7code = request.POST['i7code']
     if i7code == 'Input your I7 Source here...':
         return render(request, 'i7up/detail.html', {
                 'error_message' : "You didn't input your code!"
                 })
     else:
-        return render(request, 'i7up/detail.html', {
+        if i7code:
+            return render(request, 'i7up/detail.html', {
                 'opts' : i7.annotate(i7code.encode("utf8","ignore"))
                 })
+        else:
+            e_message = ''
+            for i, x in request.POST.iteritems():
+                try:
+                    #for lemma in x:
+                        e_message += 'Understand "' + x + '" as ' + i + '.\n'
+                except AttributeError:
+                    pass
+            #print str(request.POST);
+            return render(request, 'i7up/detail.html', {
+                'error_message' : e_message
+            })
 #        return HttpResponseRedirect(reverse('i7up:result', args=(i7code,)))
+
+def gen_page(request):
+    for key, value in request.POST:
+        return render(request, 'i7up/detail.html', {})
